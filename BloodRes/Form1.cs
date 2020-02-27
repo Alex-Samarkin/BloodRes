e55ad9b75d8dee5d038945527dc1f071c28c3b83
alex_ <alexsamarkinru@gmail.com>
2020-02-27 15:27:01 +0300

    Добавьте файлы проекта.

---
+++ BloodRes/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BloodRes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            InitResults();
+
+            dataGridView1.DataSource = Results;
+        }
+
+        public List<Res> Results { get; set; } = new List<Res>();
+
+        public void InitResults()
+        {
+            Res r = new Res();
+            /// 1
+            r.ID = 1;
+            r.Title = "Glu";
+            r.Nmin = 3.2;
+            r.Nmax = 15;
+            r.Value = 6.2;
+            Results.Add(r);
+            /// 2 
+            r = new Res(){ID = 2, Title = "Hgb",Nmin=120,Nmax = 160,Value = 130};
+            Results.Add(r);
+            /// 3
+            Results.Add(
+                    new Res() { ID = 3, Title = "HDL", Nmin = 2, Nmax = 4.8, Value = 3 }
+                    );
+        }
+    }
+}
---
+++ BloodRes/Res.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodRes
+{
+    public class Res
+    {
+        [DisplayName("Код")]
+        public int ID { get; set; } = 0;
+        [DisplayName("Имя показателя")]
+        public string Title { get; set; } = "Показатель";
+        [DisplayName("Норма (мин)")]
+        public double Nmin { get; set; } = 0;
+        [DisplayName("Норма (макс)")]
+        public double Nmax { get; set; } = 10;
+        [DisplayName("Среднее")]
+        public double Avg => (Nmax + Nmin) / 2;
+        [DisplayName("Измерено")]
+        public double Value { get; set; } = 0;
+    }
+}
